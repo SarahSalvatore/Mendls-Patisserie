e0451f55a94336f33b1128b3c14cac71b74f5534
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-07-20 16:13:24 -0400

    Creates Product Details page.

--- mendls-client/src/pages/ProductDetailPage.vue
+++ mendls-client/src/pages/ProductDetailPage.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Product Details</div>
+  <div>
+    <div class="img-wrap">
+      <img :src="product.imageName" />
+    </div>
+    <div class="product-details">
+      <h1>{{ product.name }}</h1>
+      <p class="price">{{ product.price }}</p>
+      <button class="add-to-cart">Add to Cart</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { products } from "../data/temp-data";
 export default {
   name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find(
+        (product) => product.id === this.$route.params.productId
+      ),
+    };
+  },
 };
 </script>
--- mendls-client/src/pages/ProductsPage.vue
+++ mendls-client/src/pages/ProductsPage.vue
@@ -2,11 +2,12 @@
   <div class="products-container">
     <h1>Pastries</h1>
     <div class="grid-wrap">
-      <div v-for="product in products" :key="product.id">
+      <div class="product" v-for="product in products" :key="product.id">
         <img class="product-img" :src="product.imageName" />
         <h3 class="product-name">{{ product.name }}</h3>
         <p class="product-price">{{ product.price }}</p>
         <p class="product-desc">{{ product.desc }}</p>
+        <button>Order</button>
       </div>
     </div>
   </div>
